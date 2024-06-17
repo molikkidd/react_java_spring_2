58cee078165a350c1de323bccb01ed21738dfede
Molikkidd <mzkidd5@gmail.com>
2024-06-17 18:31:06 -0400

    add s3 logic for getting and returning an object

--- backend/src/main/java/com/amigoscode/s3/S3Config.java
+++ backend/src/main/java/com/amigoscode/s3/S3Config.java
@@ -1,6 +1,7 @@
 package com.amigoscode.s3;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
@@ -11,6 +12,7 @@ public class S3Config {
     @Value("${aws.region}")
     private String awsRegion;
 
+    @Bean
     public S3Client s3Client(){
         S3Client client = S3Client.builder()
                 .region(Region.US_EAST_2)
